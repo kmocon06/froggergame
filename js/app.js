c557c614c821cb268673be50c8a2ae3fdf3c2dcc
Krystina O'Connor <krystinaoconnor@Krystinas-MacBook-Pro.local>
2020-01-02 13:02:12 -0600

    added files and got frogger image to appear at bottom center of screen

---
+++ js/app.js
@@ -0,0 +1,11 @@
+//Frogger Game
+//First create a character who is able to move in all directions 
+//(upwards, left, right, downwards)
+// class Player {
+// 	constructor(level, score)
+// }
+
+
+const froggerGame = {
+
+}
